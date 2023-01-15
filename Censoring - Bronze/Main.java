07a6238a0a335cf0cce05497b0a96f03d34199f3
Your Name <yujinpetercho@gmail.com>
2023-01-15 17:35:34 -0600

    solved Censoring - Bronze

--- Censoring - Bronze/Main.java
+++ Censoring - Bronze/Main.java
@@ -2,21 +2,12 @@ import java.util.*;
 import java.io.*;
 public class Main {
     public static String remove(String sentence, String badWord) {
-        int ib = 0;
-        int i = 0;
-        return removeHelper(sentence, badWord, i, ib);
+        String oldSentence = sentence;
+        sentence = sentence.replace(badWord, "");
+        if (oldSentence.equals(sentence)) {return null;}
+        return sentence;
     }
-    public static String removeHelper(String sentence, String badWord, int iterator, int badworditerator) {
-        if (sentence.length() - 1 == iterator) {
-            return null;
-        }
-        if (sentence.charAt(iterator) == badWord.charAt(badworditerator))  {
-            sentence.replace(badworditerator)
-            return removeHelper(sentence, badWord, iterator + 1, badworditerator + 1);
-        }else {
-            return removeHelper(sentence, badWord, iterator + 1, badworditerator);
-        }
-    }
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader r = new BufferedReader(new FileReader("Main.in"));
@@ -26,9 +17,14 @@ public class Main {
         String sentence = st.nextToken();
         st = new StringTokenizer(r.readLine()); //could we possibly remove this statement altogether? seems like a waste of good simplicity
         String badWord = st.nextToken();
-        remove(sentence, badWord);
-        
 
-        
+
+        String x = "";
+        while ((x = remove(sentence, badWord)) != null) {
+            sentence = x;
+        }
+
+        pw.println(sentence);
+        pw.close();
     }
 }
