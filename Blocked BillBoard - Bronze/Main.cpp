ee2523e1ff69dbbcf16b74f15a00807404d6c586
Yujin Cho <yujinpetercho@gmail.com>
2023-01-21 07:11:04 -0600

    .gitignore file and some more problems

---
+++ Blocked BillBoard - Bronze/Main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    freopen("Main.in", "r", stdin);
+    freopen("Main.out", "w", stdout);
+    int a;
+    int b;
+    int c;
+    cin >> a >> b >> c;
+    cout << a + b + c;
+
+}
